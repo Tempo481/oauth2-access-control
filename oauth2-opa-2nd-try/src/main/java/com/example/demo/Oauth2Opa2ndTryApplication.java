463e7e18f739a137f287f1a26b5484d3dc8fdb71
Tempo481 <matthiasadrian7@gmail.com>
2021-06-10 21:44:04 +0200

    spring security and opa / 2nd try

---
+++ oauth2-opa-2nd-try/src/main/java/com/example/demo/Oauth2Opa2ndTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Opa2ndTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Opa2ndTryApplication.class, args);
+	}
+
+}
